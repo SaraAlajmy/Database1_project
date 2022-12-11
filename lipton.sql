02441af3b32786f5ae38697f43b54077eace8769
SaraAlajmy <saraalagami@gmail.com>
2022-12-12 01:00:30 +0200

    Update lipton.sql

--- lipton.sql
+++ lipton.sql
@@ -1,19 +1,133 @@
 CREATE PROC createAllTables 
 as 
 BEGIN
-CREATE TABLE SystemUser (username varchar(20) primary key ,password_ varchar(20))
-CREATE TABLE Club(id int primary key IDENTITY, name_ varchar(20), location_ varchar(20))
-CREATE TABLE Stadium(id int primary key IDENTITY, status_ bit, name_ varchar(20), capacity int, location_ varchar(20))
-CREATE TABLE Club_Representative(id int primary key IDENTITY, name_ varchar(20), username varchar(20), foreign key(username) references SystemUser,club_ID int, foreign key(club_id) references Club(id))
-CREATE TABLE Stadium_Manager(id int primary key IDENTITY, name_ varchar(20), username varchar(20), foreign key(username) references SystemUser(username),stadium_id int,foreign key(stadium_id)references Stadium(id))
-CREATE TABLE Fan(national_id int primary key, phone_number int, name_ varchar(20), address_ varchar(20), status_ bit, birth_date date, username varchar(20), foreign key(username) references SystemUser(username))
-CREATE TABLE Sports_Association_Manager(id int primary key IDENTITY, name_ varchar(20), username varchar(20), foreign key(username) references SystemUser(username))
-CREATE TABLE System_Admin(id int primary key IDENTITY, name_ varchar(20), username varchar(20), foreign key(username) references SystemUser(username))
-CREATE TABLE Match(id int primary key IDENTITY, start_time datetime, end_time datetime,host_club_id int,guest_club_ID int,foreign key(host_club_id) references club(id),FOREIGN key(guest_club_ID) REFERENCES club(id),stadium_id int,FOREIGN key(stadium_id) REFERENCES stadium(id) )
-CREATE TABLE Ticket(id int primary key IDENTITY, status_ bit,match_id int,foreign key (match_id) references Match(id))
-CREATE TABLE Host_Request(id int primary key IDENTITY, status_ varchar(20), match_id int foreign key references match(id),manager_id int foreign key references Stadium_Manager(id),representative_id int foreign key references Club_Representative(id))
-CREATE TABLE Ticket_Buying_Transactions(fan_national_Id int foreign key references fan(national_id),ticket_id int foreign key references Ticket(id))
+CREATE TABLE SystemUser (username varchar(20) primary key ,password_ varchar(20));
+CREATE TABLE Club(id int primary key IDENTITY, name_ varchar(20), location_ varchar(20));
+CREATE TABLE Stadium(id int primary key IDENTITY, status_ bit, name_ varchar(20), capacity int, location_ varchar(20));
+CREATE TABLE Club_Representative(id int primary key IDENTITY, name_ varchar(20), username varchar(20), foreign key(username) references SystemUser,club_ID int, foreign key(club_id) references Club(id));
+CREATE TABLE Stadium_Manager(id int primary key IDENTITY, name_ varchar(20), username varchar(20), foreign key(username) references SystemUser(username),stadium_id int,foreign key(stadium_id)references Stadium(id));
+CREATE TABLE Fan(national_id int primary key, phone_number int, name_ varchar(20), address_ varchar(20), status_ bit, birth_date date, username varchar(20), foreign key(username) references SystemUser(username));
+CREATE TABLE Sports_Association_Manager(id int primary key IDENTITY, name_ varchar(20), username varchar(20), foreign key(username) references SystemUser(username));
+CREATE TABLE System_Admin(id int primary key IDENTITY, name_ varchar(20), username varchar(20), foreign key(username) references SystemUser(username));
+CREATE TABLE Match(id int primary key IDENTITY, start_time datetime, end_time datetime,host_club_id int,guest_club_ID int,foreign key(host_club_id) references club(id),FOREIGN key(guest_club_ID) REFERENCES club(id),stadium_id int,FOREIGN key(stadium_id) REFERENCES stadium(id) );
+CREATE TABLE Ticket(id int primary key IDENTITY, status_ bit,match_id int,foreign key (match_id) references Match(id));
+CREATE TABLE Host_Request(id int primary key IDENTITY, status_ varchar(20), match_id int foreign key references match(id),manager_id int foreign key references Stadium_Manager(id),representative_id int foreign key references Club_Representative(id));
+CREATE TABLE Ticket_Buying_Transactions(fan_national_Id int foreign key references fan(national_id),ticket_id int foreign key references Ticket(id));
 end
+
+EXEC createAllTables;
+GO
+CREATE PROC dropAllTables 
+as 
+Begin
+    DROP TABLE SystemUser;
+    DROP TABLE Club_Representative;
+    DROP TABLE Stadium_Manager;
+    DROP TABLE Fan;
+    DROP TABLE Sports_Association_Manager;
+    DROP TABLE System_Admin;
+    DROP TABLE Ticket;
+    DROP TABLE Club;
+    DROP TABLE Host_Request;
+    DROP TABLE Match;
+    DROP TABLE Stadium;
+    DROP table Ticket_Buying_Transactions
+end
+
+
+go
+
+-- CREATE PROC dropAllProceduresFunctionsViews
+go
+CREATE PROC clearAllTables 
+as 
+BEGIN
+    Truncate TABLE SystemUser;
+    TRUNCATE TABLE Club_Representitave;
+    TRUNCATE TABLE Stadium_Manager;
+    TRUNCATE TABLE Fan;
+    TRUNCATE TABLE Sports_Association_Manager;
+    TRUNCATE TABLE System_Admin;
+    TRUNCATE TABLE Ticket;
+    TRUNCATE TABLE Club;
+    TRUNCATE TABLE Host_Request;
+    TRUNCATE TABLE Match;
+    TRUNCATE TABLE Stadium;
+END
+
+GO
+CREATE VIEW allAssocManagers
+AS
+SELECT s.username, s.name_ 
+FROM Sports_Association_Manager s;
+go
+CREATE VIEW allClubRepresentatives
+AS
+SELECT R.username, R.name_,C.name_
+From  Club_Representative R INNER JOIN Club C ON R.club_id=C.id
+go
+Create VIEW allStadiumManagers
+AS
+SELECT M.username, M.name_, S.name_
+From Stadium_Manager M INNER JOIN Stadium S ON M.stadium_id=S.id
+go
+Create VIEW allFans
+As 
+SELECT national_id, name_, status_, birthdate
+From Fan
+go
+
+
+Create VIEW allMatches 
+AS
+SELECT c1.name_,c2.name_,c2.name_ as 'Host_Club' , m.start_time
+from Club c1,club c2, match m WHERE m.guest_club_ID=c1.id and m.host_club_id=c2.id
+go
+
+Create VieW allTickets
+As 
+SELECT c1.name_,c2.name_,s.name_,m.start_time
+from club c1 ,club c2,match m, stadium s
+where m.guest_club_ID=c1.id and m.host_club_id=c2.id and m.stadium_id =s.id
+go
+
+create view allClubs
+AS 
+select name_,location_
+from club
+GO
+
+CREATE view allStadiums
+as
+SELECT s.name_,s.location_,s.capacity,s.status
+from Stadium
+
+go
+
+
+create view allRequests
+as
+select r.name_,s.name_,h.status_
+from Club_Representative r,Stadium_Manager s,Host_Request h
+where h.manager_id=s.id and h.representative_id=r.id
+
+go
+
+CREATE proc addAssociationManager 
+@name_ varchar(20),
+@username varchar(20),
+@pass varchar(20)
+as
+begin
+insert into SystemUser VALUES (@username,@pass)
+insert into Sports_Association_Manager values (@name_,@username)
+END
+
+GO
+
+create proc addNewMatch
+@club1_name varchar(20)
+
 Go 
 
 create procedure addStadium 
